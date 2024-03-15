882a91600732b75b8760d41a5c1767aa1c017e85
ReinDC <rein_delacruz@dlsu.edu.ph>
2024-03-15 14:38:23 +0800

    Server files and js file for login page

---
+++ login.js
@@ -0,0 +1,40 @@
+const user = function(username, password, email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+}
+
+let users = [];
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("login-form");
+    const errorElement = document.getElementById("error");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    
+    form.addEventListener("submit", (e) => {
+        let messages = [];
+
+        if(username.value === '' || username.value === null) {
+            messages.push("Username must be provided!")
+        }
+
+        else if(password.value != 'correct') {
+            messages.push("Wrong Password!")
+        }
+
+        if(messages.length > 0) {            
+            document.getElementById("error").classList.add("show"); // Shows the e
+            errorElement.innerText = messages.join(", ");
+            e.preventDefault();
+        }
+
+    });
+
+    document.getElementById("regisBtn").onclick = function() {
+        location.href = "register.html";
+    };
+
+});
