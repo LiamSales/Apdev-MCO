ba6b7a0b52805dd9bcd928c574eb533f71f006f1
ReinDC <rein_delacruz@dlsu.edu.ph>
2024-03-31 22:35:02 +0800

    Update login.js

--- views/js/login.js
+++ views/js/login.js
@@ -4,33 +4,65 @@ const user = function(username, password, email) {
     this.email = email;
 }
 
+let users = [{
+        "ID": 1,
+        "email": "roimarc_bilbao@dlsu.edu.ph",
+        "username": "roboboboi",
+        "password":"adudu",
+    },{
+        "ID": 2,
+        "email": "rein_delacruz@dlsu.edu.ph",
+        "username": "rainraingoaway",
+        "password":"combackanotherday",
+    },{
+        "ID": 3,
+        "email": "kyle@gmail.com",
+        "username": "kryo",
+        "password": "fireelement",
+    }, {
+        "ID": 4,
+        "email": "arjay@gmail.com",
+        "username": "Arcane",
+        "password":"tftlover",
+    },{
+        "ID": 5,
+        "email": "angel@hotmail.com",
+        "username": "DoctorWhoLover",
+        "password":"weepingangels",
+    }];
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("login-form");
-    const errorElement = document.getElementById("error");
     const username = document.getElementById("username");
     const password = document.getElementById("password");
+
+    function loginValidation() {
+        var username = document.forms["login-form"]["username"].value;
+        var password = document.forms["login-form"]["password"].value;
+    
+        if (username.trim() === "" || password.trim() === "") {
+            alert("Please fill in all fields.");
+            return false;
+        } else if (password.trim() != confirmPassword.trim()) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        
+        return true; 
+    }
     
     form.addEventListener("submit", (e) => {
-        let messages = [];
-
-        if(username.value === '' || username.value === null) {
-            messages.push("Username must be provided!")
-        }
-
-        else if(password.value != 'correct') {
-            messages.push("Wrong Password!")
-        }
-
-        if(messages.length > 0) {            
-            document.getElementById("error").classList.add("show"); // Shows the e
-            errorElement.innerText = messages.join(", ");
-            e.preventDefault();
-        }
+        
 
     });
 
     document.getElementById("regisBtn").onclick = function() {
         location.href = "register.html";
     };
+
+
 
 });
