90f4167642d8cf0c55cb169627ae366e90b7d879
ReinDC <rein_delacruz@dlsu.edu.ph>
2024-04-03 02:39:57 +0800

    Comments

--- script/databaseInitialize.js
+++ script/databaseInitialize.js
@@ -3,13 +3,12 @@ const RestaurantSchema = require('../src/models/Restaurants.js');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-
+// This just initializes the database in your local machine
 
 
 async function initialize() {
     try {
         await mongoose.connect('mongodb://localhost:27017/'+ process.env.DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
